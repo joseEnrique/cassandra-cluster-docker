2e2f4f382ee634b69327287d4bde4ea74abcfe4c
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2017-08-16 09:13:24 +0200

    changes about version

--- fromscratch/scripts/cassandra-clusternode.sh
+++ fromscratch/scripts/cassandra-clusternode.sh
@@ -23,23 +23,19 @@ else
         sed -i -e "s/^endpoint_snitch:.*/endpoint_snitch: $CASSANDRA_SNITCH/" $CASSANDRA_CONFIG/cassandra.yaml
 fi
 
-if [ -z "$CASSANDRA_BROADCAST" ]; then
-        echo "No BROADCAST specified, by default it is disabled"
-else
-        sed -i -e "s/^\# broadcast_rpc_address: 1.2.3.4/broadcast_rpc_address: $CASSANDRA_BROADCAST/" $CASSANDRA_CONFIG/cassandra.yaml
 
-fi
 # Dunno why zeroes here
-sed -i -e "s/^rpc_address.*/rpc_address: $ALL/" $CASSANDRA_CONFIG/cassandra.yaml
+sed -i -e "s/^rpc_address.*/rpc_address: $IP/" $CASSANDRA_CONFIG/cassandra.yaml
+sed -i -e "s/^\# broadcast_rpc_address: 1.2.3.4/broadcast_rpc_address: $IP/" $CASSANDRA_CONFIG/cassandra.yaml
 # Listen on IP:port of the container
-sed -i -e "s/^listen_address.*/listen_address: $IP/" $CASSANDRA_CONFIG/cassandra.yaml
+sed -i -e "s/^listen_address.*/listen_address: /" $CASSANDRA_CONFIG/cassandra.yaml
 
 # Configure Cassandra seeds
 if [ -z "$CASSANDRA_SEEDS" ]; then
 	echo "No seeds specified, being my own seed..."
 	CASSANDRA_SEEDS=$SEEDS
 fi
-sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$CASSANDRA_SEEDS\"/" $CASSANDRA_CONFIG/cassandra.yaml
+sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$IP,$CASSANDRA_SEEDS\"/" $CASSANDRA_CONFIG/cassandra.yaml
 
 # With virtual nodes disabled, we need to manually specify the token
 if [ -z "$CASSANDRA_TOKEN" ]; then
@@ -52,9 +48,7 @@ sed -i -e "s/^\#num_tokens: 256/num_tokens: $CASSANDRA_TOKEN/" $CASSANDRA_CONFIG
 if ! grep -q "auto_bootstrap: false" $CASSANDRA_CONFIG/cassandra.yaml; then
   sed -i '1s/^/auto_bootstrap: false\n/' $CASSANDRA_CONFIG/cassandra.yaml
 fi
-
-echo "MAX_HEAP_SIZE=\"2GB\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
-
+cp /etc/cassandra/cassandra.yml /prueba
 #echo "JVM_OPTS=\"\$JVM_OPTS -Dcassandra.initial_token=$CASSANDRA_TOKEN\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
 
 # Most likely not needed
